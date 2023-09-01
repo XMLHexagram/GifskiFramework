c4745a79863296e9cfb1e1a42f28e4c85c155045
runner <runner@Mac-1693584078540.local>
2023-09-01 16:10:53 +0000

    [no ci] Bump tdlib 1.11.0

--- Package.swift
+++ Package.swift
@@ -1,24 +1,21 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.7
+// DO NOT EDIT! Generated automatically. See scripts/swift_package_generator.py
 import PackageDescription
 
 let package = Package(
-    name: "Gifski",
+    name: "TDLibFramework",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Gifski",
             targets: ["Gifski"]
-        ),
+        )
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "Gifski",
-            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.8.16-470c36ce/TDLibFramework.zip",
-            checksum: "4bb76120da4394e4f543f080af73e912883662fe9a8a75dea5e8a9ed8e7aa65f"
-        )
+            url: "https://github.com/XMLHexagram/GifskiFramework/releases/download/1.11.0/GifskiFramework.zip",
+            checksum: "3fec39bfa2729d20c16e6fd726896b604c94842017f8ebb892c2118f3212635f"
+        ),
     ]
 )

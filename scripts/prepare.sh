82caec26a39cbfff53da03532a5b08520f00bd85
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2023-09-01 23:25:52 +0800

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,6 +4,7 @@ set -e
 cd "$( dirname "$0" )" && cd ..|| exit 1
 
 #rustup toolchain remove nightly-aarch64-apple-darwin
+rustup toolchain install nightly
 rustup install nightly
 rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
 rustup target add aarch64-apple-darwin

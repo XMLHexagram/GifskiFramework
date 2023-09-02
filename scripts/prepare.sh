73949c3050835df03f47feef49907202de9ac755
hexagram <xml.hexagram@gmail.com>
2023-09-02 12:48:40 +0800

    Fix CI

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,11 +3,13 @@ set -e
 
 cd "$( dirname "$0" )" && cd ..|| exit 1
 
-#rustup toolchain remove nightly-aarch64-apple-darwin
-# rustup toolchain install nightly
 rustup install nightly
-# rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
-rustup component add rust-src --toolchain nightly-x86_64-apple-darwin
+arch=$(uname -m)
+if [[ $arch == x86_64* ]]; then
+    rustup component add rust-src --toolchain nightly-x86_64-apple-darwin
+elif  [[ $arch == arm* ]]; then
+    rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
+fi
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-ios-sim
 rustup target add aarch64-apple-ios
